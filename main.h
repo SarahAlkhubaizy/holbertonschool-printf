c788791034ccc91d633c68fdc944d78ed29d7714
SarahAlkhubaizy <sarahalkhubaizy@gmail.com>
2026-04-12 13:35:34 +0000

    Add File

---
+++ main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+
+int _printf(const char *format, ...);
+int handle_char(va_list args);
+int handle_string(va_list args);
+
+#endif /* MAIN_H *i/
