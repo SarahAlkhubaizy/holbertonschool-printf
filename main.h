3016d86c1065e7a850af08e2126e06e1bf6b4d49
ReemaAlshahrani <reema.alshahrani.ali@gmail.com>
2026-04-14 14:27:23 +0000

    Update all files

--- main.h
+++ main.h
@@ -2,11 +2,14 @@
 #define MAIN_H
 
 #include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 int _printf(const char *format, ...);
+int _putchar(char c);
+
 int handle_char(va_list args);
 int handle_string(va_list args);
 int handle_int(int n);
-int _putchar(char c);
-
+int handle_int_helper(unsigned int num);
 #endif
