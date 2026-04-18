a1293b9731e6ad41ae3866cda12d9a3b9adcbf1b
ReemaAlshahrani <reema.alshahrani.ali@gmail.com>
2026-04-18 19:58:58 +0000

    Implement local 1024-byte buffer and update all handlers for printf

--- _printf.c
+++ _printf.c
@@ -1,44 +1,41 @@
 #include "main.h"
-#include <unistd.h>
-#include <stdarg.h>
 
 /**
- * check_specifier - handles format specifier
- * @c: the specifier character
- * @args: argument list
+ * check_specifier - routes the format specifier to the correct handler
+ * @c: the specifier character (e.g., 'd', 's', 'c')
+ * @args: the list of variable arguments
+ * @buffer: local buffer of 1024 chars
+ * @index: current position in the buffer
  *
- * Return: number of characters printed
+ * Return: number of characters handled
  */
-int check_specifier(char c, va_list args)
+int check_specifier(char c, va_list args, char *buffer, int *index)
 {
-	int count;
+	int count = 0;
 
-	count = 0;
 	if (c == 'c')
-		count = handle_char(args);
+		count = handle_char(args, buffer, index);
 	else if (c == 's')
-		count = handle_string(args);
+		count = handle_string(args, buffer, index);
 	else if (c == 'd' || c == 'i')
-		count = handle_int(args);
+		count = handle_int(args, buffer, index);
 	else if (c == 'b')
-                count = handle_binary(args);
+		count = handle_binary(args, buffer, index);
 	else if (c == 'u')
-                count = handle_unsigned(args);
-        else if (c == 'o')
-                count = handle_octal(args);
-        else if (c == 'x')
-                count = handle_hex(args, 0);
-        else if (c == 'X')
-                count = handle_hex(args, 1);
+		count = handle_unsigned(args, buffer, index);
+	else if (c == 'o')
+		count = handle_octal(args, buffer, index);
+	else if (c == 'x')
+		count = handle_hex(args, 0, buffer, index);
+	else if (c == 'X')
+		count = handle_hex(args, 1, buffer, index);
 	else if (c == '%')
-	{
-		write(1, "%", 1);
-		count = 1;
-	}
+		count = write_to_buffer('%', buffer, index);
 	else
 	{
-		write(1, "%", 1);
-		write(1, &c, 1);
+		/* If specifier is unknown, print the '%' and the character */
+		write_to_buffer('%', buffer, index);
+		write_to_buffer(c, buffer, index);
 		count = 2;
 	}
 	return (count);
@@ -48,38 +45,43 @@ int check_specifier(char c, va_list args)
  * _printf - produces output according to a format
  * @format: character string with directives
  *
- * Return: number of characters printed
+ * Return: total number of characters printed
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    char buffer[BUF_SIZE];
-    int i = 0, count = 0, buf_idx = 0;
+	va_list args;
+	char buffer[BUF_SIZE];
+	int i = 0, count = 0, buf_idx = 0;
 
-    if (!format) return (-1);
-    va_start(args, format);
+	if (!format)
+		return (-1);
 
-    while (format && format[i])
-    {
-        if (format[i] != '%')
-        {
-            count += write_to_buffer(format[i], buffer, &buf_idx);
-        }
-        else
-        {
-            i++;
-            if (!format[i])
-            {
-                va_end(args);
-                return (-1);
-            }
-	    
-	    count += handle_specifier(format[i], args, buffer, &buf_idx);
-        }
-        i++;
-    }
+	va_start(args, format);
+	while (format && format[i])
+	{
+		if (format[i] != '%')
+		{
+			/* Add normal characters to buffer */
+			count += write_to_buffer(format[i], buffer, &buf_idx);
+		}
+		else
+		{
+			i++; /* Move to the character after '%' */
+			if (!format[i])
+			{
+				va_end(args);
+				return (-1);
+			}
+			/* Pass everything to the specifier checker */
+			count += check_specifier(format[i], args, buffer, &buf_idx);
+		}
+		i++;
+	}
 
-    write(1, buffer, buf_idx);
-    va_end(args);
-    return (count);
+	/* IMPORTANT: Print any remaining characters in the buffer before exiting */
+	if (buf_idx > 0)
+		write(1, buffer, buf_idx);
+
+	va_end(args);
+	return (count);
 }
